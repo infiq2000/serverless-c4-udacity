1b2a1b5b96140a58967b9ed1c74d67677eb4edd3
dat <datht@example.com>
2023-08-28 01:23:47 +0700

    add business logic

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,98 +1,40 @@
-import * as AWS from 'aws-sdk'
-const AWSXRay = require('aws-xray-sdk')
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import * as uuid from 'uuid'
+import { TodoAccess } from '../dataLayer/todoDB'
 import { createLogger } from '../utils/logger'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate'
+import { AttachmentUtils } from '../helpers/attachmentUtils'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 
-const XAWS = AWSXRay.captureAWS(AWS)
-const logger = createLogger('todoAccess')
+const logger = createLogger('TodoAccess')
+const attachmentUtils = new AttachmentUtils()
+const todoAccess = new TodoAccess()
 
-export class TodoAccess {
-  constructor(
-    private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
-    private readonly todosTable = process.env.TODOS_TABLE
-  ) {}
+export const getTodosForUser = async (userId: string) => {
+  return todoAccess.getTodos(userId)
+}
 
-  async getTodos(userId: string): Promise<TodoItem[]> {
-    logger.info('Getting all todo items')
+export const createTodo = async (userId: string, todo: CreateTodoRequest) => {
+  const todoId = uuid.v4()
+  logger.info(`Creating todo ${todoId}`)
+  const attachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
+  return todoAccess.createTodo({
+    userId,
+    todoId,
+    createdAt: new Date().toISOString(),
+    done: false,
+    attachmentUrl,
+    ...todo
+  })
+}
 
-    const result = await this.docClient
-      .query({
-        TableName: this.todosTable,
-        KeyConditionExpression: 'userId = :userId',
-        ExpressionAttributeValues: {
-          ':userId': userId
-        }
-      })
-      .promise()
-    return result.Items as TodoItem[]
-  }
+export const updateTodo = async (
+  userId: string,
+  todoId: string,
+  todo: UpdateTodoRequest
+) => {
+  return todoAccess.updateTodo(userId, todoId, todo)
+}
 
-  async createTodo(newTodo: TodoItem): Promise<TodoItem> {
-    logger.info(`Creating new todo item: ${newTodo.todoId}`)
-    await this.docClient
-      .put({
-        TableName: this.todosTable,
-        Item: newTodo
-      })
-      .promise()
-    return newTodo
-  }
-
-  async updateTodo(
-    userId: string,
-    todoId: string,
-    updateData: TodoUpdate
-  ): Promise<void> {
-    logger.info(`Updating a todo item: ${todoId}`)
-    await this.docClient
-      .update({
-        TableName: this.todosTable,
-        Key: { userId, todoId },
-        ConditionExpression: 'attribute_exists(todoId)',
-        UpdateExpression: 'set #n = :n, dueDate = :due, done = :dn',
-        ExpressionAttributeNames: { '#n': 'name' },
-        ExpressionAttributeValues: {
-          ':n': updateData.name,
-          ':due': updateData.dueDate,
-          ':dn': updateData.done
-        }
-      })
-      .promise()
-  }
-
-  async deleteTodo(userId: string, todoId: string): Promise<void> {
-    await this.docClient
-      .delete({
-        TableName: this.todosTable,
-        Key: { userId, todoId }
-      })
-      .promise()
-  }
-
-  async saveImgUrl(
-    userId: string,
-    todoId: string,
-    bucketName: string
-  ): Promise<void> {
-    try {
-      await this.docClient
-        .update({
-          TableName: this.todosTable,
-          Key: { userId, todoId },
-          ConditionExpression: 'attribute_exists(todoId)',
-          UpdateExpression: 'set attachmentUrl = :attachmentUrl',
-          ExpressionAttributeValues: {
-            ':attachmentUrl': `https://${bucketName}.s3.amazonaws.com/${todoId}`
-          }
-        })
-        .promise()
-      logger.info(
-        `Updating image url for a todo item: https://${bucketName}.s3.amazonaws.com/${todoId}`
-      )
-    } catch (error) {
-      logger.error(error)
-    }
-  }
+export const deleteTodo = async (userId: string, todoId: string) => {
+  return todoAccess.deleteTodo(userId, todoId)
 }
